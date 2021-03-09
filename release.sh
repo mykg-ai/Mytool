9f615266c4df30e9d48420af26a0216a48d7625a
goddy <wuchuansheng@yeah.net>
2021-03-09 14:03:47 +0800

    update

--- release.sh
+++ release.sh
@@ -1,1 +1,6 @@
-mvn clean package deploy
+export GPG_TTY=$(tty)
+#mvn clean package deploy
+#mvn clean javadoc:jar deploy -P release
+#mvn clean deploy -P release -Dgpg.passphrase=
+#mvn clean deploy -P release
+mvn clean deploy
